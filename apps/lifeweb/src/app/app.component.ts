4c98d52d04b109d4feb7f44f2beb368ce1067792
brian seim <microsoft@seim.me>
2020-08-14 18:17:14 -0500

    initial commit

---
+++ apps/lifeweb/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'evodlife-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lifeweb';
+}
