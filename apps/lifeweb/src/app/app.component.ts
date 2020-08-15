50dd202df13b7635d7b3aef45ee15b11511ce9fa
brian seim <microsoft@seim.me>
2020-08-15 15:58:05 -0500

    Added Simple Google Authentication

--- apps/lifeweb/src/app/app.component.ts
+++ apps/lifeweb/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'evodlife-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lifeweb';
+  items: Observable<any[]>;
+  get currentUser() {
+    return this.authService.auth.user;
+  };
+
+  constructor(firestore: AngularFirestore, private authService: AuthService) {
+    this.items = firestore.collection('items').valueChanges();
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- apps/lifeweb/src/app/app.module.ts
+++ apps/lifeweb/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
+import {environment} from "../environments/environment";
+import {AuthService} from "./services/auth.service";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
