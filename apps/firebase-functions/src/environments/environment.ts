403aefc1137d2eecbb8e49633aa79820c006c0d1
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-28 15:50:19 +0800

    local setup

--- apps/firebase-functions/src/environments/environment.ts
+++ apps/firebase-functions/src/environments/environment.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBwCofAItOTYTgjQXnU4Z8wdOZji88w8xc',
+    authDomain: 'yesmomiapp.firebaseapp.com',
+    databaseURL: 'https://yesmomiapp.firebaseio.com',
+    projectId: 'yesmomiapp',
+    storageBucket: 'yesmomiapp.appspot.com',
+    messagingSenderId: '803645533857',
+    appId: '1:803645533857:web:d24b6f50b7d36ae997cae0',
+    measurementId: 'G-THYYZNSDWE',
+  },
 };
--- apps/lifeweb/src/app/app.module.ts
+++ apps/lifeweb/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AppComponent} from './app.component';
-import {environment} from "../environments/environment";
-import {AuthService} from "./services/auth.service";
-import {AppRoutingModule, routingComponents} from "./app-routing.module";
-import {SharedModule} from "primeng/api";
-import {MemberModule} from "./member/member.module";
-import {MemberRoutingModule} from "./member/member-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {RippleModule} from "primeng/ripple";
-import {CommonModule} from "@angular/common";
-import {MenubarModule} from "primeng/menubar";
-import {ButtonModule} from "primeng/button";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AppComponent } from './app.component';
+// import {environment} from "../environments/environment";
+import { environment } from '../../../firebase-functions/src/environments/environment';
+import { AuthService } from './services/auth.service';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { SharedModule } from 'primeng/api';
+import { MemberModule } from './member/member.module';
+import { MemberRoutingModule } from './member/member-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
@@ -36,5 +37,4 @@ import {ButtonModule} from "primeng/button";
   providers: [AuthService],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
